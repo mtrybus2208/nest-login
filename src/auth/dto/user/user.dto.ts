c89d0f677698b2c6a2332e29ac4e3cc9ec6a208d
mtrybus2208 <mtrybus2208@wp.pl>
2021-02-13 20:33:16 +0100

    Add global validation pipe

--- src/auth/dto/user/user.dto.ts
+++ src/auth/dto/user/user.dto.ts
@@ -1,8 +1,40 @@
+import { 
+  IsNotEmpty,
+  MaxLength,
+  IsDefined,
+  MinLength,
+  IsNumber,
+} from 'class-validator';
+
 import { UserGender } from '../../types/user/user.types';
+
+export enum UserMessage {
+  INVALID_USER_NAME_REQUIRED = 'User name is required',
+  INVALID_USER_NAME_MIN = 'User name must be minimum 4 characters long',
+  INVALID_USER_NAME_MAX = 'User name must be maximum  100 characters long',
+  INVALID_USER_AGE_TYPE = 'You must specify a number',
+  INVALID_USER_AGE_REQUIRED = 'User age is required',
+}
 
 export class UserDto {
   id?: string;
+
+  @MinLength(4, {
+    message: UserMessage.INVALID_USER_NAME_MIN,
+  })
+  @MaxLength(100, {
+    message: UserMessage.INVALID_USER_NAME_MAX,
+  })
+  @IsNotEmpty({
+    message: UserMessage.INVALID_USER_NAME_REQUIRED,
+  })
+  @IsDefined()
   userName: string;
+
   gender: UserGender;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsDefined()
   age: number;
 }
--- src/auth/entity/user.entity.ts
+++ src/auth/entity/user.entity.ts
@@ -2,18 +2,6 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 import { UserGender } from '../types/user/user.types';
 
-interface Message {
-  [key: string]: string;
-}
-
-export const message: Message = {
-  INVALID_USER_NAME_REQUIRED: 'User name is required',
-  INVALID_USER_NAME_MIN: 'User name must be minimum 4 characters long',
-  INVALID_USER_NAME_MAX: 'User name must be maximum  20 characters long',
-  INVALID_USER_AGE_TYPE: 'You must specify a number',
-  INVALID_USER_AGE_REQUIRED: 'User age is required',
-};
-
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
